356b0785f3d8f7ad5a6d38f95ee077787bf93b81
xyzker <410317669@qq.com>
2025-09-19 10:52:53 +0800

    update

--- eks_ec2.tf
+++ eks_ec2.tf
@@ -58,6 +58,49 @@ resource "aws_instance" "t2_micro" {
   }
 }
 
+# --- EKS Managed Node Group ---
+resource "aws_iam_role" "eks_node_group_role" {
+  name = "eksNodeGroupRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_eks_node_group" "demo_node_group" {
+  cluster_name    = aws_eks_cluster.eks-demo.name
+  node_group_name = "demo-node-group"
+  node_role_arn   = aws_iam_role.eks_node_group_role.arn
+  subnet_ids      = [aws_subnet.eks_subnet_a.id, aws_subnet.eks_subnet_b.id]
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+  instance_types = ["t2.micro"]
+}
+
 # --- IRSA for EKS: Service Account with S3 List Permission ---
 
 data "aws_eks_cluster" "eks" {
