0564de8dc68b81cda2da9c33e9965b341e98b655
xyzker <410317669@qq.com>
2025-09-19 09:45:19 +0800

    add eks

---
+++ eks_ec2.tf
@@ -0,0 +1,53 @@
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "eks_subnet" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+}
+
+resource "aws_eks_cluster" "demo" {
+  name     = "demo-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.eks_subnet.id]
+  }
+}
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eksClusterRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_instance" "t2_micro" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI for us-east-1
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.eks_subnet.id
+
+  tags = {
+    Name = "t2-micro-instance"
+  }
+}
